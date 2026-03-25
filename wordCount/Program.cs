7df07acdffc98b01fe4aabc8d5a32b707111543d
User <user@example.com>
2026-03-25 20:32:54 +0500

    Добавлены разделители, текст

--- wordCount/Program.cs
+++ wordCount/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+    public static char[] Separators = {
+    ' ', '\t', '\n', '\r',
+      '.', ',', '!', '?', ';', ':', '-',
+    '(', ')', '[', ']', '{', '}',
+    '«', '»',
+    '—', '–',
+    '/', '\\',
+    '"', '\'', '`',
+    '_', '*', '#', '@', '&', '%', '+', '=',
+    '<', '>', '|', '^', '~' };
+    private static void Main(string[] args)
+    {
+        var filePath = Path.Combine("..", "..", "..", "text.txt");
+
+
+        if (!File.Exists(filePath))
+        {
+            return;
+        }
+        var textList = new List<string>();
+        using (var reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                textList.Add(line);
+            }
+        }
+        var text = textList.ToArray();
+
+        string word = "он";
+    }
+}
